f71d447c0afb0431bb43bd1ce77ce93be4c75fae
VNS <sarathchand09@gmail.com>
2018-05-11 17:14:06 +0530

    empty spring boot project

---
+++ src/main/java/com/workout/j9/j9workout/J9WorkoutApplication.java
@@ -0,0 +1,12 @@
+package com.workout.j9.j9workout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J9WorkoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J9WorkoutApplication.class, args);
+	}
+}
---
+++ src/test/java/com/workout/j9/j9workout/J9WorkoutApplicationTests.java
@@ -0,0 +1,16 @@
+package com.workout.j9.j9workout;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class J9WorkoutApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
